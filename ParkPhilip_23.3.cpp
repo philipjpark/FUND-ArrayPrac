a1d89853ae47da2612292886335dd61d34f9fc8b
philipjpark <66290921+philipjpark@users.noreply.github.com>
2022-02-07 00:16:38 -0500

    First commit

---
+++ ParkPhilip_23.3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+string allOdd(int a[][2], int x, int y) {
+    for (int i = 0; i < x; i++)
+      for (int j = 0; j < 2; j++)
+        if (a[i][j] % 2 == 0) return "Not All Odd";
+
+    return "All Odd";
+}
+
+int main() {
+int x[2][2] = {{1,3}, {5,7}};
+cout<< allOdd(x,2,2) << endl;
+
+return 0;
+}
